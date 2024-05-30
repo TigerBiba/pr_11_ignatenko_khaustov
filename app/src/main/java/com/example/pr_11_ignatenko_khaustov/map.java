8b10a4bd90875d323f01512f992c0521db8ba794
Mickey <artplay1995@gmail.com>
2024-05-30 14:07:53 +0700

    norva bobr

---
+++ app/src/main/java/com/example/pr_11_ignatenko_khaustov/map.java
@@ -0,0 +1,28 @@
+package com.example.pr_11_ignatenko_khaustov;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.content.Intent;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class map extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_map);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+                Uri.parse("http://maps.google.com/maps?saddr=70.916,09.18&daddr=94.666,23.840"));
+        startActivity(intent);
+    }
+}
